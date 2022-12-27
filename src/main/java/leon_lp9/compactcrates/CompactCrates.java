eea2da327e4ea127551705e699d2231841b5ad51
Leon_lp9 <leon.marcel.rabe@gmail.com>
2022-12-27 22:19:40 +0100

    Initial commit

---
+++ src/main/java/leon_lp9/compactcrates/CompactCrates.java
@@ -0,0 +1,17 @@
+package leon_lp9.compactcrates;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CompactCrates extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
