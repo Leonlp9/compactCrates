a6f3a546e0b78e3b323b4484b745eec05089588e
Leon_lp9 <leon.marcel.rabe@gmail.com>
2022-12-27 22:45:03 +0100

    Config

--- src/main/java/leon_lp9/compactcrates/CompactCrates.java
+++ src/main/java/leon_lp9/compactcrates/CompactCrates.java
@@ -1,17 +1,50 @@
 package leon_lp9.compactcrates;
 
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public final class CompactCrates extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
 
+    File newConfig;
+    FileConfiguration newConfigz;
+
+    public void onEnable(){
+        createNewConfig();
+
+        getChestConfig().set("test", "test");
+        getConfig().set("test", "test");
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+
+    public void onDisable(){
+        saveConfig();
+        saveNewConfig();
     }
+
+    public void createNewConfig(){
+        newConfig = new File(getDataFolder(), "chests.yml");
+        newConfigz = YamlConfiguration.loadConfiguration(newConfig);
+        saveNewConfig();
+    }
+
+    public void saveNewConfig(){
+        try{
+            newConfigz.save(newConfig);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public FileConfiguration getChestConfig(){
+        return newConfigz;
+    }
+
+
 }
